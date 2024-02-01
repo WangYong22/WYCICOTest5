19fd4c73530102f1511311f799714c3aa6ead300
WangYong22 <102638006+WangYong22@users.noreply.github.com>
2024-02-01 15:55:58 +0800

    Initial Commit

---
+++ WYCICOTest5/WYCICOTest5App.swift
@@ -0,0 +1,17 @@
+//
+//  WYCICOTest5App.swift
+//  WYCICOTest5
+//
+//  Created by wy on 2024/2/1.
+//
+
+import SwiftUI
+
+@main
+struct WYCICOTest5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
