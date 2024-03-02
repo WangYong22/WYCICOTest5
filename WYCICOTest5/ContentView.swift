3d0b6c0175fa2bfb391f5f9c8ae0a1c4375b8b9f
WangYong22 <102638006+WangYong22@users.noreply.github.com>
2024-03-02 12:15:28 +0800

    hello 2

--- WYCICOTest5/ContentView.swift
+++ WYCICOTest5/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, 1 world!")
+            Text("Hello, 2 world!")
         }
         .padding()
     }
